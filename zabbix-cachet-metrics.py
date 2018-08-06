e6b4576ec507db144e19d72d8fc27ac9e0c7576b
shiyunjin <luoyibin_001@163.com>
2018-08-06 18:52:00 +0800

    修改配置文件格式,测试成功

--- zabbix-cachet-metrics.py
+++ zabbix-cachet-metrics.py
@@ -1,6 +1,8 @@
 import sys
-
+import os
+import time
 import json
+import logging
 import requests
 from datetime import datetime, timedelta
 
@@ -8,17 +10,21 @@ try:
     if os.getenv('CONFIG_FILE') is not None:
         config_content = open(os.environ['CONFIG_FILE'])
     else:
-		config_content = open(os.path.dirname(os.path.realpath(__file__))+'/config.json')
+        config_content = open(os.path.dirname(os.path.realpath(__file__))+'/config.json')
 except Exception as e:
     print(e)
     sys.exit(0)
 else:
     config_dict = json.load(config_content)
 
-cachethq_url = config_dict['config_main']['cachethq_url']
-cachethq_api_key = config_dict['config_main']['cachethq_api_key']
-update_comp_interval = config_dict['update_comp_interval']
-
+cachet_url      = config_dict['service']['cachet']['url']
+cachet_api_key  = config_dict['service']['cachet']['api_key']
+zbx_url         = config_dict['service']['zbx']['url']
+zbx_user        = config_dict['service']['zbx']['username']
+zbx_pass        = config_dict['service']['zbx']['password']
+zbx_api_url     = zbx_url + '/api_jsonrpc.php'
+zbx_token       = None
+update_interval = config_dict['update_interval']
 
 def get_datetime():
     datetime_now = datetime.today().strftime("%Y-%m-%d %H:%M:00")
@@ -53,39 +59,47 @@ def zbx_login(zbx_url, zbx_usrname, zbx_pawd):
     return req_content
 
 
-def zbx_logout(zbx_url, zbx_login_id, zbx_login_token):
-    payload = {
-        "jsonrpc": "2.0",
-        "method": "user.logout",
-        "params": [],
-        "id": zbx_login_id,
-        "auth": zbx_login_token
-    }
+def get_zbx_item_value(zbx_url, zbx_token, zbx_item_type, zbx_item_id):
+    if zbx_item_type == 'host': 
+        payload = {
+            "jsonrpc": "2.0",
+            "method": "history.get",
+            "params": {
+                "output": "extend",
+                "history": 3,
+                "itemids": zbx_item_id,
+                "sortfield": "clock",
+                "sortorder": "DESC",
+                "limit": 1
+            },
+            "auth": zbx_token,
+            "id": 1
+        }
+    else:
+        payload = {
+            "jsonrpc": "2.0",
+            "method": "history.get",
+            "params": {
+                "output": "extend",
+                "history": 0,
+                "itemids": zbx_item_id,
+                "sortfield": "clock",
+                "sortorder": "DESC",
+                "limit": 1
+            },
+            "auth": zbx_token,
+            "id": 1
+        }
     headers = {'content-type': 'application/json'}
     req_run = requests.post(zbx_url, data=json.dumps(payload), headers=headers)
     req_content = json.loads(req_run.text)
-    return req_content
-
-
-def get_zbx_item_value(zbx_url, zbx_token, zbx_item_id):
-	payload = {
-		"jsonrpc": "2.0",
-		"method": "history.get",
-		"params": {
-			"output": "extend",
-			"history": 0,
-			"itemids": zbx_item_id,
-			"sortfield": "clock",
-			"sortorder": "DESC",
-			"limit": 1
-		},
-		"auth": zbx_token,
-		"id": 1
-	}
-    headers = {'content-type': 'application/json'}
-    req_run = requests.post(zbx_url, data=json.dumps(payload), headers=headers)
-    req_content = json.loads(req_run.text)
-    req_value = req_content['result'][0]['value']
+    if zbx_item_type == 'host': 
+        if req_content['result'][0]['value'] != "0":
+            req_value = 0
+        else:
+            req_value = 100
+    else:
+        req_value = req_content['result'][0]['value']
     return str(req_value)
 
 
@@ -126,7 +140,7 @@ def get_number_of_visits(es_url, es_index_name, es_gte, es_lte):
 
 
 def cachethq_metrics_add_point(api_token, metric_id, metric_value, metric_timestamp):
-    req_url = cachethq_url + '/api/v1/metrics/' + str(metric_id) + '/points'
+    req_url = cachet_url + '/api/v1/metrics/' + str(metric_id) + '/points'
     if metric_timestamp is None:
         payload = {
             "value": metric_value
@@ -144,17 +158,12 @@ def cachethq_metrics_add_point(api_token, metric_id, metric_value, metric_timest
 
 
 def run_zbx(config_json, time_now):
-    zbx_api_url = config_json['zbx_api_url'] + '/api_jsonrpc.php'
-    zbx_username = config_json['zbx_username']
-    zbx_passwd = config_json['zbx_passwd']
-    zbx_item_id = config_json['zbx_id']
+    zbx_type = config_json['type']
+    zbx_id = config_json['id']
     metric_id = config_json['metric_id']
 
-    login_content = zbx_login(zbx_api_url, zbx_username, zbx_passwd)
-    zbx_token = login_content['result']
-    item_value = get_zbx_item_value(zbx_api_url, zbx_token, zbx_item_id)
-    zbx_logout(zbx_api_url, login_content['id'], zbx_token)
-    cachethq_metrics_add_point(cachethq_api_key, metric_id, item_value, time_now[0:10])
+    item_value = get_zbx_item_value(zbx_api_url, zbx_token, zbx_type, zbx_id)
+    cachethq_metrics_add_point(cachet_api_key, metric_id, item_value, time_now[0:10])
 
 
 def run_es6(config_json, time_now, time_old):
@@ -163,11 +172,10 @@ def run_es6(config_json, time_now, time_old):
     metric_id = config_json['metric_id']
 
     item_value = get_number_of_visits(es6_api_url, es6_index, time_old, time_now)
-    cachethq_metrics_add_point(cachethq_api_key, metric_id, item_value, time_now[0:10])
+    cachethq_metrics_add_point(cachet_api_key, metric_id, item_value, time_now[0:10])
 
 
 def run_main():
-	exit_status = 0
     timestamp_dict = get_datetime()
     timestamp_now = timestamp_dict['timestamp_now']
     timestamp_old = timestamp_dict['timestamp_old']
@@ -180,13 +188,18 @@ def run_main():
 
 
 if __name__ == "__main__":
-    while True:
-		run_main()
-		time.sleep(update_comp_interval)
-	except KeyboardInterrupt:
-		event.set()
-		logging.info('Shutdown requested. See you.')
-	except Exception as error:
-		logging.error(error)
-		exit_status = 1
-	sys.exit(exit_status)
+    exit_status = 0
+    try:
+        login_content = zbx_login(zbx_api_url, zbx_user, zbx_pass)
+        zbx_token = login_content['result']
+
+        while True:
+            run_main()
+            time.sleep(update_interval)
+
+    except KeyboardInterrupt:
+        logging.info('Shutdown requested. See you.')
+    except Exception as error:
+        logging.error(error)
+        exit_status = 1
+    sys.exit(exit_status)
